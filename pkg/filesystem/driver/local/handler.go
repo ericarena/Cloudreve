389c4d006c5aad43fa3aeb80b456ae65c5e47683
ericarena <to_eric@163.com>
2022-04-26 16:35:12 +0800

    add local driver rename move handler

--- pkg/filesystem/driver/local/handler.go
+++ pkg/filesystem/driver/local/handler.go
@@ -280,3 +280,41 @@ func (handler Driver) Token(ctx context.Context, ttl int64, uploadSession *seria
 func (handler Driver) CancelToken(ctx context.Context, uploadSession *serializer.UploadSession) error {
 	return nil
 }
+
+// 重命名文件
+func (handler Driver) Rename(ctx context.Context, src, dst string) error {
+	err := os.Rename(src, dst)
+	if err != nil {
+		util.Log().Warning("无法重命名文件，%s", err)
+		return err
+	}
+	return nil
+}
+
+// 复制文件
+func (handler Driver) Copy(ctx context.Context, src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		util.Log().Warning("不是常规文件，%s", err)
+		return 0, err
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
